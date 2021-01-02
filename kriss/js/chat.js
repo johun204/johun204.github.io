17863c2b77af4d9afc21e33bc9770c1bef708de2
Dong Hun Cho <johun204@gmail.com>
2021-01-02 23:44:03 +0900

    Add files via upload

--- kriss/js/chat.js
+++ kriss/js/chat.js
@@ -1,17 +1,36 @@
 window.onresize = function(){
   try{
-    document.getElementById("chat_btn").style.bottom = document.getElementById("chat_room").offsetHeight + "px";
+    if(chat_state == 1){
+      document.getElementById("chat_btn").style.bottom = document.getElementById("chat_room").offsetHeight + "px";
+    }
   }catch(err){ }
 }
 function ChatBtnClick(obj)
 {
-  if(obj.innerHTML == "채팅 열기"){
-    document.getElementById("chat_room").style.height = "50%";
+  var chat_state = localStorage.getItem("chat_state");
+  if(chat_state == "1"){
+    localStorage.setItem("chat_state", "0");
+    document.getElementById("chat_btn").style.bottom = "0px";
+    document.getElementById("chat_room").style.display = "none";
+    obj.innerHTML = "채팅 열기";
+  }else{
+    localStorage.setItem("chat_state", "1");
+    document.getElementById("chat_room").style.display = "block";
     document.getElementById("chat_btn").style.bottom = document.getElementById("chat_room").offsetHeight + "px";
     obj.innerHTML = "채팅 닫기";
-  }else{
-    document.getElementById("chat_room").style.height = "0px";
+  }
+}
+function ChatOnload()
+{
+  var chat_state = localStorage.getItem("chat_state");
+  console.log(chat_state);
+  if(chat_state == "1"){
+    document.getElementById("chat_room").style.display = "block";
     document.getElementById("chat_btn").style.bottom = document.getElementById("chat_room").offsetHeight + "px";
-    obj.innerHTML = "채팅 열기";
+    document.getElementById("chat_btn_btn").innerHTML = "채팅 닫기";
+  }else{
+    document.getElementById("chat_btn").style.bottom = "0px";
+    document.getElementById("chat_room").style.display = "none";
+    document.getElementById("chat_btn_btn").innerHTML = "채팅 열기";
   }
 }
