8c3899a65f3abb0cb070a2ae9687d9bcd226a7cf
Dong Hun Cho <johun204@gmail.com>
2021-02-15 19:46:27 +0900

    Add files via upload

--- kriss/js/restore.js
+++ kriss/js/restore.js
@@ -3,7 +3,7 @@ function restore(txt){
   var crack2 = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "󰡔", "󰡕", "", "", "", "", "", "", "", "", "", "", ""];
   try{
     for(var i=0;i<crack.length;i++){
-      txt = txt.split(crack[i]["c"]).join(" ");
+      txt = txt.split(crack[i]["c"]).join(crack[i]["o"]);
     }for(var i=0;i<crack2.length;i++){
       txt = txt.split(crack2[i]).join(" ");
     }
