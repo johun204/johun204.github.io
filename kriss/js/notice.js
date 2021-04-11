184ef66d3e76b2cb8d7193bf417cf244014ac618
Dong Hun Cho <johun204@gmail.com>
2021-04-11 16:07:19 +0900

    Add files via upload

--- kriss/js/notice.js
+++ kriss/js/notice.js
@@ -1,6 +1,6 @@
 function openNotice(){
   const notice_date = "20210411";
-  const notice_text = "<table class='table table-sm table-borderless'><tr><td><b>2021.04.11</b></td><td>단어&문장 라벨링 시 일부 device가 누락되는 오류 수정. <sup class='new-badge'>NEW</sup></td></tr><tr><td><b>2021.03.30</b></td><td>다크모드 기능 추가.</td></tr><tr><td><b>2021.03.09</b></td><td>줄 번호 제거 메뉴 추가.</td></tr></table>";
+  const notice_text = "<table class='table table-sm table-borderless'><tr><td><b>2021.04.11</b></td><td>단어&문장 라벨링 시 일부 device 태그가 누락되던 오류를 수정했습니다. <sup class='new-badge'>NEW</sup></td></tr><tr><td><b>2021.03.30</b></td><td>다크 모드 기능을 추가했습니다.</td></tr><tr><td><b>2021.03.09</b></td><td>줄 번호 제거 기능을 추가했습니다.</td></tr></table>";
 
   try{
     document.getElementById("modal-text").innerHTML = notice_text;
