51056ef0e3bc675d45dfc2c2d5ec378ed06c22fd
Dong Hun Cho <johun204@gmail.com>
2021-04-11 14:02:58 +0900

    Add files via upload

---
+++ kriss/js/notice.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+  const notice_date = "20210411";
+  const notice_text = "<table class='table table-sm table-borderless'><tr><td><b>2021.04.11</b></td><td>단어&문장 라벨링 시 일부 device가 누락되는 오류 수정.</td></tr><tr><td><b>2021.03.30</b></td><td>다크모드 기능 추가.</td></tr></table>";
+
+  try{
+    document.getElementById("modal-text").innerHTML = notice_text;
+    if(localStorage.getItem("notice") != notice_date){
+      localStorage.setItem("notice", notice_date);
+      $('#noticeModal').modal('show');
+    }
+  }catch(err){}
+}
