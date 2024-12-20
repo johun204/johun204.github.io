636885e50f5ba238691e8ba33e82a79ee4c0579f
Dong Hun Cho <johun204@gmail.com>
2024-12-21 01:27:40 +0900

    Add files via upload

---
+++ study/service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker 설치됨!!!!!!');
+    event.waitUntil(
+        caches.open('quiz-cache').then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './icon-192x192.png',
+                './icon-512x512.png',
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
