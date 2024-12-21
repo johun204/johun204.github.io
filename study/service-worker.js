add83405ee63bb7184275edfffbdf63683611d60
Dong Hun Cho <johun204@gmail.com>
2024-12-21 10:43:39 +0900

    Add files via upload

--- study/service-worker.js
+++ study/service-worker.js
@@ -1,12 +1,28 @@
 // service-worker.js
-self.addEventListener("install", event => {
-    console.log("Service Worker installing...");
+
+self.addEventListener('install', (event) => {
+    console.log('Service Worker 설치됨!!!!!!');
+    event.waitUntil(
+        caches.open('quiz-cache').then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './icon-192x192.png',
+                './icon-512x512.png',
+            ]);
+        })
+    );
 });
 
-self.addEventListener("activate", event => {
-    console.log("Service Worker activated.");
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
 
-self.addEventListener("fetch", event => {
-    console.log("Fetching:", event.request.url);
+self.addEventListener('activate', event => {
+    console.log('Service Worker activated.');
 });
